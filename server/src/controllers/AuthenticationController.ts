64c1684160740c8b271627bc60c66027ebf91bfd
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-12-31 17:34:03 +0200

    add basic authentication controller

---
+++ server/src/controllers/AuthenticationController.ts
@@ -0,0 +1,27 @@
+import { Response, Request, NextFunction } from "express";
+
+export class AuthenticationController {
+  register = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  login = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  logout = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      
+    } catch (error) {
+      next(error)
+    }
+  }
+}
