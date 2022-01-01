d6d7f96294eb40ffdefb3e4a587faef5bc7935de
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2022-01-01 16:41:16 +0200

    add register and login functions

--- server/src/controllers/AuthenticationController.ts
+++ server/src/controllers/AuthenticationController.ts
@@ -1,9 +1,19 @@
 import { Response, Request, NextFunction } from "express";
+import bcrypt from 'bcryptjs' 
+import { User } from "../models/User";
+import { ITokenHelper } from "../interfaces/ITokenHelper";
 
 export class AuthenticationController {
+  tokenHelper: ITokenHelper
+  constructor(tokenHelper: ITokenHelper){
+    this.tokenHelper = tokenHelper
+  }
   register = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      
+      const { name, password, email } = req.body;
+      const hashedPass = await bcrypt.hash(password, 12);
+      await User.create({ email, name, password: hashedPass });
+      res.status(200).send({ message: 'user is registered. please login' })
     } catch (error) {
       next(error)
     }
@@ -11,7 +21,14 @@ export class AuthenticationController {
 
   login = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      
+      const { password, email } = req.body;
+      const user = await User.findOne({ where: { email } });
+      if (!user) throw new Error('wrong credentials');
+      const isPasswordCorrect = await bcrypt.compare(password, user.password);
+      if (!isPasswordCorrect) throw new Error('wrong credentials');
+      const accessToken = this.tokenHelper.signToken(user.id, 'access');
+      const refreshToken = this.tokenHelper.signToken(user.id, 'refresh');
+      return res.status(200).send({ accessToken, refreshToken });
     } catch (error) {
       next(error)
     }
