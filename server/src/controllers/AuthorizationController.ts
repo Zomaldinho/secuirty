29dc1fc0207adfa6177720e90b5a0606c315d1ac
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2022-01-01 23:40:29 +0200

    add authorization controller

---
+++ server/src/controllers/AuthorizationController.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Response } from "express";
+import { AuthRequest } from "../interfaces/IAuthRequest";
+import { Action } from "../models/Action";
+
+export class AuthorizationController {
+  validatePermission = async (req: AuthRequest, res: Response, next: NextFunction) => {
+    try {
+      const { actionName } = req.body;
+      const action = await Action.findOne({ where: { name: actionName } })
+      if(!action) throw new Error("can't find requested action")
+      const userActions = await req.user!.getActions()
+      const isAuthorized = userActions.includes(action)
+      return res.status(200).send({ isAuthorized })
+    } catch (error) {
+      next(error)
+    }
+  }
+}
