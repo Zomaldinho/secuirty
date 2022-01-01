d68b79e55238e3c029811e9afd98d1d02794c309
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2022-01-01 23:37:32 +0200

    add request body validators

---
+++ server/src/validators/RequestsBodyValidator.ts
@@ -0,0 +1,40 @@
+import { check } from "express-validator";
+
+export const registerValidator = () => {
+  return [
+    check('name').exists().withMessage('name is required'),
+    check('email')
+      .exists()
+      .withMessage('email is required')
+      .isEmail()
+      .withMessage('wrong format'),
+    check('password')
+      .exists()
+      .withMessage('password is required')
+      .isLength({ min: 6 })
+      .withMessage('password min length is 6 chars'),
+  ];
+};
+
+export const loginValidator = () => {
+  return [
+    check('email')
+      .exists()
+      .withMessage('email is required')
+      .isEmail()
+      .withMessage('wrong format'),
+    check("password").exists().withMessage("password is required"),
+  ];
+};
+
+export const refreshTokenValidator = () => {
+  return [
+    check("refreshToken").exists().withMessage("refreshToken is required"),
+  ]
+}
+
+export const validateActionValidator = () => {
+  return [
+    check("actionName").exists().withMessage("actionName is required"),
+  ]
+}
