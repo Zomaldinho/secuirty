10489a34d740e3d9a20cb2f8ebd14c7b4f8fa3f3
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2022-01-04 00:25:03 +0200

    format code

--- server/src/controllers/AuthorizationController.ts
+++ server/src/controllers/AuthorizationController.ts
@@ -1,18 +1,24 @@
-import { NextFunction, Response } from "express";
-import { AuthRequest } from "../interfaces/IAuthRequest";
-import { Action } from "../models/Action";
+import { NextFunction, Response } from 'express';
+import { AuthRequest } from '../interfaces/IAuthRequest';
+import { Action } from '../models/Action';
 
 export class AuthorizationController {
-  validatePermission = async (req: AuthRequest, res: Response, next: NextFunction) => {
+  validatePermission = async (
+    req: AuthRequest,
+    res: Response,
+    next: NextFunction
+  ) => {
     try {
       const { actionName } = req.body;
-      const action = await Action.findOne({ where: { name: actionName } })
-      if(!action) throw new Error("can't find requested action")
-      const userActions = await req.user!.getActions()
-      const isAuthorized = userActions.some(element => element.name == action.name)
-      return res.status(200).send({ isAuthorized })
+      const action = await Action.findOne({ where: { name: actionName } });
+      if (!action) throw new Error("can't find requested action");
+      const userActions = await req.user!.getActions();
+      const isAuthorized = userActions.some(
+        (element) => element.name == action.name
+      );
+      return res.status(200).send({ isAuthorized });
     } catch (error) {
-      next(error)
+      next(error);
     }
-  }
+  };
 }
